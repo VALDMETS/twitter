b98c74479d48b8d8e8ae36b467ba2eb2314f8549
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-07-19 13:52:19 -0500

    getting groundwork

---
+++ app/scripts/settings.js
@@ -0,0 +1,6 @@
+export default {
+  baseUrl: 'https://bass.kinvey.com',
+  appKey: 'kid_Hyh6Ex2P',
+  appSecret: '93abd41a8a704036a214b079be90d77c',
+  basicAuth: bota('kid_Hyh6Ex2P:93abd41a8a704036a214b079be90d77c')
+};
